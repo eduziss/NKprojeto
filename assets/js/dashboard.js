13ce3dd02d834b30867c79f7fc4ce81cc492ed32
Eduardo <eassis652@gmail.com>
2023-10-31 22:59:14 -0300

    V 0.7

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -10,11 +10,6 @@ document.querySelector("#qtde-pendente").innerHTML = qtdePendente
     document.querySelector("#qtde-concluida").innerHTML = qteConcluida
 
 
-    const totalPontos =  tarefas.reduce((total, t) => total += +1, 0 )
-    document.querySelector("#total-pontos").innerHTML = totalPontos
-
-
-
     const meusPontos =  concluida.reduce((total, t) => total += +t.quantidade_arquivos, 0 )
     document.querySelector("#meus-pontos").innerHTML = meusPontos
 
